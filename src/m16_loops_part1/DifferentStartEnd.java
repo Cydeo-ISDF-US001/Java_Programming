c75f3fab2eed2dee89e7568483ee4da83a2c5373
CundullahT-CT <cterzioglu@cybertekschool.com>
2023-10-07 13:23:35 +0200

    Module16 Loops Part1

---
+++ src/m16_loops_part1/DifferentStartEnd.java
@@ -0,0 +1,59 @@
+package m16_loops_part1;
+
+public class DifferentStartEnd {
+
+    public static void main(String[] args) {
+
+        for (int i = 5; i < 21; i++) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+
+        System.out.println("--------------------------------");
+
+        for (int i = 20; i > 10; i--) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+
+        System.out.println("--------------------------------");
+
+        for (int i = 10; i <= 100; i += 10) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+
+        System.out.println("--------------------------------");
+
+        for (int i = 50; i > 10; i -= 4) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+
+        System.out.println("--------------------------------");
+
+        for (int i = 5; i < 10; i++) {
+            System.out.println("Hello");
+        }
+
+
+    }
+
+
+}
+
+/*
+Print the numbers which starts from 5 and ends at 20 (inclusive) in the same line with a space between the numbers.
+
+Print the numbers which starts from 20 and ends at 10 (exclusive).
+
+Print the numbers which starts from 10 and ends at 100 (inclusive) with an increment of 10.
+
+Print the numbers which starts from 50 and ends at 10 (exclusive) with and decrement of 4.
+
+Print "Hello" five times.
+ */
