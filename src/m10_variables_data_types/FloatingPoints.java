26a15bed4dabc5145c0b9d6729e4d77fca17778a
CundullahT-CT <cterzioglu@cybertekschool.com>
2023-10-07 13:19:33 +0200

    Module10 Variables and Data Types

---
+++ src/m10_variables_data_types/FloatingPoints.java
@@ -0,0 +1,19 @@
+package m10_variables_data_types;
+
+public class FloatingPoints {
+
+    public static void main(String[] args) {
+
+        float maxFloat = 3.14F;
+        double maxDouble;
+
+       //System.out.println(maxDouble);
+
+        maxDouble = 3.14;
+
+        System.out.println(maxFloat);
+        System.out.println(maxDouble);
+
+    }
+
+}
